7088b603fa1752e2a206cef451adbe312b63d978
moinur-rahman <rahmanmoin27@gmail.com>
2022-10-09 21:21:44 +0600

    Netify and Render Launch

---
+++ server/src/app.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+require("./db/mongoose");
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "..", "client", "build", "index.html")
+    );
+  });
+}
+
+module.exports = app;
---
+++ server/src/routers/task.js
@@ -0,0 +1,119 @@
+const express = require("express");
+const Task = require("../models/task");
+const auth = require("../middleware/auth");
+const router = express.Router();
+
+router.post("/tasks", auth, async (req, res) => {
+  const task = new Task({
+    ...req.body,
+    owner: req.user._id,
+  });
+
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+//GET /tasks?completed=true
+//GET /tasks?limit=10&skip=0
+//GET /tasks?sortBy=createdAt:desc
+router.get("/tasks", auth, async (req, res) => {
+  const match = {};
+  const sort = {};
+  const limit = req.query.limit ? parseInt(req.query.limit) : 8;
+  const skip = req.query.skip ? parseInt(req.query.skip) : 0;
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === "true";
+  }
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(":");
+    sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
+  }
+
+  try {
+    await req.user.populate({
+      path: "tasks",
+      match,
+      options: {
+        limit,
+        skip,
+        sort,
+      },
+    });
+
+    const totalTasks = await Task.find({ owner: req.user._id });
+
+    res.send({
+      tasks: req.user.tasks,
+      totalTasks: totalTasks.length,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
+});
+
+router.get("/tasks/:id", auth, async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const task = await Task.findOne({ _id, owner: req.user._id });
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+router.patch("/tasks/:id", auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["description", "completed"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  try {
+    const task = await Task.findOne({
+      _id: req.params.id,
+      owner: req.user._id,
+    });
+
+    if (!task) {
+      return res.status(404).send();
+    }
+    updates.forEach((update) => (task[update] = req.body[update]));
+
+    await task.save();
+
+    res.send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.delete("/tasks/:id", auth, async (req, res) => {
+  try {
+    const task = await Task.findOneAndDelete({
+      _id: req.params.id,
+      owner: req.user._id,
+    });
+    if (!task) {
+      res.status(404).send();
+    }
+    res.send(task);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+module.exports = router;
