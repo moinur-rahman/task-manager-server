5ddf669dbedc2328e2c8b08270b1f5786a550955
moinur-rahman <rahmanmoin27@gmail.com>
2022-10-10 00:48:50 +0600

    process.env.NODE_ENV == production remove in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -12,14 +12,15 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "..", "client", "build", "index.html")
-    );
-  });
-}
-
 module.exports = app;
+
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+
+//   app.get("*", (req, res) => {
+//     res.sendFile(
+//       path.resolve(__dirname, "..", "client", "build", "index.html")
+//     );
+//   });
+// }
